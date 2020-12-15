e7b4a1a8ed619f0e772d981318dc1b63f1fc0e5f
Abdulhakeem Dahir <somlutions@gmail.com>
2020-12-14 16:02:56 -0800

    setting up inquirer

--- app.js
+++ app.js
@@ -15,25 +15,27 @@ const render = require("./lib/htmlRenderer");
 
 // Ask user for manager info
 
-function askUserForManagerInfo(){
-
+function askUserForManagerInfo() {
+  return inquirer.prompt([
+    {
+      message: "Name",
+      name: "name",
+      type: "input",
+    },
+  ]).then((response) => {
+      
+  });
 }
 
 // Ask use for next employee type
 
-function askUserForEmployeeInfo() {
-
-}
+function askUserForEmployeeInfo() {}
 
 // Ask user for engineer info
-function askUserForEngineerInfo() {
-
-}
+function askUserForEngineerInfo() {}
 
 // Ask user for intern info
-function askUserForInternInfo() {
-    
-}
+function askUserForInternInfo() {}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
